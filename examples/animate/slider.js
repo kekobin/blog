718bdf855ecf10ae3bbe219bb86b512e57a56482
Jimmy <kekobin@163.com>
2019-07-20 20:48:24 +0800

    add anotation for slider.js

--- examples/animate/slider.js
+++ examples/animate/slider.js
@@ -18,7 +18,7 @@ class Slider {
 		// reset wraper style
 		this.el.style.position = 'relative';
 		this.wraper.style.position = 'absolute';
-		// 复制第一张图片item，添加到wraper后。
+		// (1)复制第一张图片item，添加到wraper后。这个是用来实现无限切换的一种方式。
         const newItem = this.wraper.children[0].cloneNode(true);
         this.wraper.appendChild(newItem);
 
@@ -28,6 +28,11 @@ class Slider {
 	}
 
 	autoPlay() {
+		// (2)当切换完最后一张复制的item后到下一次新的切换之前，重置索引index到第二张(注意不是第一张)，
+		// 由于(1)中复制item的存在，因为它和第一张是一样的，所以当此时切换的时候，可以无痕的掠过第一张
+		// (比如说perWidth为100,那么到切换完复制item时，wraper.style.left=-100*6px=-600px,
+		// 此时设置wraper.style.left=0,即由第六张位置切换到第一张位置，由于图片一样，并且这个切换是非常快速无动画的，看不出来
+		// 由此实现了无限切换的效果。
 		if (++this.index > this.length) {
 			this.index = 1;
 			this.wraper.style.left = 0;
