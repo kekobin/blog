1ce8e2825318a233a65dda9b2405dd867c52a6fa
kebin <kebin@bigo.sg>
2019-10-30 16:15:09 +0800

    add rollup starter

---
+++ examples/rollup-starter/src/index.js
@@ -0,0 +1,5 @@
+import { add, sub } from './util';
+
+export default function test() {
+	return add(10,10)
+};
---
+++ examples/rollup-starter/src/util.js
@@ -0,0 +1,15 @@
+export const add = (a, b) => {
+	return a + b;
+};
+
+export const sub = (a, b) => {
+	return a - b;
+};
+
+export const multiplicate = (a, b) => {
+	return a * b;
+};
+
+export const division = (a, b) => {
+	return a / b;
+};
